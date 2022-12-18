5b9aedcc91799b74452e897cd6517b0cb9118f32
Seoyun0208 <sylph0105@naver.com>
2022-12-18 15:37:21 +0900

    [ADD] 3.3 props (함수형 컴포넌트)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import MyComponent from './MyComponent';
 
 const App = () => {
   return (
-    <MyComponent/>
+    <>
+    <MyComponent name="React" title="props = React"/>
+    <MyComponent title="defaultProps = 리액트"/>
+    <MyComponent title="children = 리액트">리액트</MyComponent>
+    <MyComponent title="favoriteNumber = 2" favoriteNumber={2}/>
+    </>
   );
 };
 
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const MyComponent = () => {
+const MyComponent = ({title, name, children, favoriteNumber}) => {
+    // const {title, name, children} = props;
     return (
         <div>
-            나의 새롭고 멋진 컴포넌트
+            TITLE: {title}<br/>
+            안녕하세요, 제 이름은 {name}입니다.<br/>
+            {children && <span>children 값은 {children}입니다.</span>}<br/>
+            {favoriteNumber && <span>제가 좋아하는 숫자는 {favoriteNumber}입니다.</span>}<br/>
+            ----------------------------------------
         </div>
     );
+};
+
+MyComponent.defaultProps = {
+    name: '리액트'
+};
+
+MyComponent.propTypes = {
+    name: propTypes.string,
+    favoriteNumber: propTypes.number.isRequired
 };
 
 export default MyComponent;
