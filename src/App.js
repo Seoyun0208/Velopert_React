86e85b7be363acd559ee0995bf7babb1f7381edc
Seoyun0208 <sylph0105@naver.com>
2022-12-18 15:15:50 +0900

    [ADD] 3.2 첫 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import { Component } from 'react';
+import MyComponent from './MyComponent';
 
-class App extends Component {
-  render(){
-    const name = 'react';
-    return (
-      <div className="react">
-        {name}
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <MyComponent/>
+  );
+};
 
 export default App;
