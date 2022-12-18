50f575c38b570f77364d3d7f7664da42073498c8
Seoyun0208 <sylph0105@naver.com>
2022-12-18 15:41:34 +0900

    [ADD] 3.3 props (클래스형 컴포넌트)

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import { Component } from 'react';
 import propTypes from 'prop-types';
 
-const MyComponent = ({title, name, children, favoriteNumber}) => {
-    // const {title, name, children} = props;
-    return (
-        <div>
+class MyComponent extends Component{
+    static defaultProps = {
+        name: '리액트'
+    };
+    static propTypes = {
+        name: propTypes.string,
+        favoriteNumber: propTypes.number.isRequired
+    };
+
+    render() {
+        const {title, name, children, favoriteNumber} = this.props;
+        return (
+            <div>
             TITLE: {title}<br/>
             안녕하세요, 제 이름은 {name}입니다.<br/>
             {children && <span>children 값은 {children}입니다.</span>}<br/>
             {favoriteNumber && <span>제가 좋아하는 숫자는 {favoriteNumber}입니다.</span>}<br/>
             ----------------------------------------
         </div>
-    );
+        );
+    }
 };
 
-MyComponent.defaultProps = {
-    name: '리액트'
-};
+// MyComponent.defaultProps = {
+//     name: '리액트'
+// };
 
-MyComponent.propTypes = {
-    name: propTypes.string,
-    favoriteNumber: propTypes.number.isRequired
-};
+// MyComponent.propTypes = {
+//     name: propTypes.string,
+//     favoriteNumber: propTypes.number.isRequired
+// };
 
 export default MyComponent;
