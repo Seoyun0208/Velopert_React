69fa13ddf737f0d0eaa8b4051384a82d6c0c9876
Seoyun0208 <sylph0105@naver.com>
2023-01-01 19:48:32 +0900

    [ADD] 5.3 컴포넌트 자체에 ref 설정 (클래스형 컴포넌트)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
+import { Component } from 'react';
 // import MyComponent from './components/MyComponent';
 // import Counter from './components/Counter';
 // import Say from './components/Say';
 // import EventPractice from './components/EventPractice';
-import ValidationSample from './components/ValidationSample';
+// import ValidationSample from './components/ValidationSample';
+import ScrollBox from './components/ScrollBox';
 
-const App = () => {
-  return (
-    <>
-      {/* <MyComponent name="React" title="props = React"/> */}
-      {/* <MyComponent title="defaultProps = 리액트"/> */}
-      {/* <MyComponent title="children = 리액트">리액트</MyComponent> */}
-      {/* <MyComponent title="favoriteNumber = 2" favoriteNumber={2}/> */}
-      {/* <Counter/> */}
-      {/* <Say/> */}
-      {/* <EventPractice/> */}
-      <ValidationSample/>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        {/* <MyComponent name="React" title="props = React"/> */}
+        {/* <MyComponent title="defaultProps = 리액트"/> */}
+        {/* <MyComponent title="children = 리액트">리액트</MyComponent> */}
+        {/* <MyComponent title="favoriteNumber = 2" favoriteNumber={2}/> */}
+        {/* <Counter/> */}
+        {/* <Say/> */}
+        {/* <EventPractice/> */}
+        {/* <ValidationSample/> */}
+        <ScrollBox ref={ref => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨밑으로</button>
+      </>
+    );
+  }
 };
 
 export default App;
