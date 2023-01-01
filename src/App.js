67abee6ecf189a12c32660120194feb33cc07b55
Seoyun0208 <sylph0105@naver.com>
2023-01-01 17:17:43 +0900

    [ADD] 4.2 onChange 이벤트 핸들링 (클래스형 컴포넌트)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 // import MyComponent from './components/MyComponent';
 // import Counter from './components/Counter';
-import Say from './components/Say';
+// import Say from './components/Say';
+import EventPractice from './components/EventPractice';
 
 const App = () => {
   return (
     <>
-    {/* <MyComponent name="React" title="props = React"/> */}
-    {/* <MyComponent title="defaultProps = 리액트"/> */}
-    {/* <MyComponent title="children = 리액트">리액트</MyComponent> */}
-    {/* <MyComponent title="favoriteNumber = 2" favoriteNumber={2}/> */}
-    {/* <Counter/> */}
-    <Say/>
+      {/* <MyComponent name="React" title="props = React"/> */}
+      {/* <MyComponent title="defaultProps = 리액트"/> */}
+      {/* <MyComponent title="children = 리액트">리액트</MyComponent> */}
+      {/* <MyComponent title="favoriteNumber = 2" favoriteNumber={2}/> */}
+      {/* <Counter/> */}
+      {/* <Say/> */}
+      <EventPractice/>
     </>
   );
 };
