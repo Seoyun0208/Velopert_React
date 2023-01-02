69bb9c455e38ad27526800152e523777eaa6ca97
Seoyun0208 <sylph0105@naver.com>
2023-01-02 21:31:23 +0900

    [ADD] 7.3 라이프사이클 컴포넌트 구현 (클래스형 컴포넌트)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,23 @@ import { Component } from 'react';
 // import EventPractice from './components/EventPractice';
 // import ValidationSample from './components/ValidationSample';
 // import ScrollBox from './components/ScrollBox';
-import IterationSample from './components/IterationSample';
+// import IterationSample from './components/IterationSample';
+import LifeCycleSample from './components/LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({ color: getRandomColor() });
+  }
+
   render() {
     return (
       <>
@@ -22,7 +36,9 @@ class App extends Component {
         {/* <ValidationSample/> */}
         {/* <ScrollBox ref={ref => this.scrollBox = ref} />
         <button onClick={() => this.scrollBox.scrollToBottom()}>맨밑으로</button> */}
-        <IterationSample />
+        {/* <IterationSample /> */}
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
       </>
     );
   }
