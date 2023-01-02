562791212f68528e155d8e667157c8c197014392
Seoyun0208 <sylph0105@naver.com>
2023-01-02 22:18:44 +0900

    [ADD] 8.2 useEffect (함수형 컴포넌트)

--- src/components/hooks/Info.js
+++ src/components/hooks/Info.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
 
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다.');
+        console.log({name, nickname});
+    });
+
+    useEffect(() => {
+        console.log('마운트될 때만 실행됩니다.');
+    }, []);
+
+    useEffect(() => {
+        console.log('name 이 변경될 때만 실행됩니다. : ' + name);
+        return () => {
+            console.log('cleanup : ' + name);
+        }
+    }, [name]);
 
     const onChangeName = e => {
         setName(e.target.value);
